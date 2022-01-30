df4cf1e068c9c47c21ed419bbbf8edaf4b1e35ab
Vitor Severo <vitor.severo@hypeflame.com.br>
2022-01-30 17:49:20 -0300

    feat: Using Java consumer to store the lambda expression result

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,12 +16,23 @@ public class Main {
         new Thread(() -> System.out.println("Hello World!")).start();
 
         // Lambda function to print each list element
+        System.out.println("\nUse a lamba expression in the ArrayList's forEach() method to print every item in the list");
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.add(5);
         numbers.add(9);
         numbers.add(8);
         numbers.add(1);
         numbers.forEach(System.out::println);
+
+        // Using Java's Consumer interface to store a lambda expression in a variable
+        System.out.println("\nUsing Java's Consumer interface to store a lambda expression in a variable");
+        ArrayList<Integer> integerArrayList = new ArrayList<Integer>();
+        integerArrayList.add(5);
+        integerArrayList.add(9);
+        integerArrayList.add(8);
+        integerArrayList.add(1);
+        Consumer<Integer> method = System.out::println;
+        integerArrayList.forEach(method);
 
     }
 }
